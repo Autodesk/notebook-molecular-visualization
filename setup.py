fca814a8d5ee61a2969a8066f30cc9f6ddab0273
Aaron Virshup <aaronvirshup@julies-iphone.ads.autodesk.com>
2016-05-20 12:14:07 -0700

    Update and fix setup

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ from setuptools import find_packages, setup
 from setuptools.command.install import install
 import versioneer
 
+PACKAGE_NAME = 'nbmolviz'
+
 assert sys.version_info[:2] == (2, 7), "Sorry, this package requires Python 2.7."
 
 
@@ -28,13 +30,17 @@ PYEXT = set('.py .pyc .pyo'.split())
 with open('requirements.txt', 'r') as reqfile:
     requirements = [x.strip() for x in reqfile if x.strip()]
 
-def find_package_data():
+
+def find_package_data(pkgdir):
+    """ Just include all files that won't be included as package modules.
+    """
     files = []
-    for root, dirnames, filenames in os.walk('nbmolviz'):
+    for root, dirnames, filenames in os.walk(pkgdir):
+        not_a_package = '__init__.py' not in filenames
         for fn in filenames:
             basename, fext = os.path.splitext(fn)
-            if fext not in PYEXT:
-                files.append(relpath(join(root, fn), 'nbmolviz'))
+            if not_a_package or (fext not in PYEXT) or ('static' in fn):
+                files.append(relpath(join(root, fn), pkgdir))
     return files
 
 
@@ -51,17 +57,18 @@ cmdclass = versioneer.get_cmdclass()
 cmdclass['install'] = PostInstall
 
 setup(
-    name='Notebook Molecular Visualization Library',
+    name=PACKAGE_NAME,
     version=versioneer.get_version(),
     packages=find_packages(),
     classifiers=CLASSIFIERS.split('\n'),
     url='http://autodeskresearch.com',
     license='Apache 2.0',
     cmdclass=cmdclass,
-    package_data={'nbmolviz': find_package_data()},
+    package_data={PACKAGE_NAME: find_package_data(PACKAGE_NAME)},
     install_requires=requirements,
     author='Autodesk BioNano Research',
     author_email='aaron.virshup [at] autodesk [dot] com',
-    description='WebGL-based molecular visualization tools for Jupyter notebooks'
+    description='The Notebook Molecular Visualization Library: WebGL-based molecular visualization '
+                'tools for Jupyter notebooks'
 )
 
