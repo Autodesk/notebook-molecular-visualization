b64ba8c94519a9c2c1d307fe43b10089bcd7bea2
Justin McCandless <justinjmccandless@gmail.com>
2016-10-27 12:56:53 -0700

    Real test!  Thanks to fixing circular mdt import

--- nbmolviz/_tests/test_widget2d.py
+++ nbmolviz/_tests/test_widget2d.py
@@ -1,5 +1,9 @@
+from nbmolviz.utils import translate_color
+
 def func(x):
     return x + 1
 
 def test_example():
-    assert func(3) == 4
+    css_color = 'tomato'
+    translated_color = translate_color(css_color)
+    assert translated_color == '0xff6347'
