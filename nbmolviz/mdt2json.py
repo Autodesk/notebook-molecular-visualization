339aa36f86436735c57a3aadfab3cdedd89ca34e
Aaron Virshup <aaronvirshup@Gibarian.local>
2016-11-02 20:37:51 -0700

    Move MDT JSON converter into NBMolViz

---
+++ nbmolviz/mdt2json.py
@@ -0,0 +1,44 @@
+# Copyright 2016 Autodesk Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+def convert(mol):
+    from moldesign import units as u
+
+    js = dict(name=mol.name)
+
+    js['atoms'] = [{'serial': i,
+                    'name': atom.name,
+                    'elem': atom.elem,
+                    'mass_magnitude': atom.mass.value_in(u.amu),
+                    'residue_index': atom.residue.index,
+                    'residue_name': atom.residue.name,
+                    'chain': atom.chain.name,
+                    'positions': list(atom.position.value_in(u.angstrom))}
+                   for i, atom in enumerate(mol.atoms)]
+
+    js['bonds'] = [{'atom1_index': bond.a1.index,
+                    'atom2_index': bond.a2.index,
+                    'bond_order': bond.order}
+                   for i, bond in enumerate(mol.bonds)]
+
+    js['residues'] = [{'name': residue.name,
+                       'sequence_number': residue.pdbindex,
+                       'chain_index': residue.chain.index}
+                      for i, residue in enumerate(mol.residues)]
+
+    js['chains'] = [{'name': chain.name,
+                     'description': ''}
+                    for i, chain in enumerate(mol.chains)]
+    return js
